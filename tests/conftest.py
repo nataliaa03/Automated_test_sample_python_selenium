2d671699706f7c0bbaeb9d5ded7afda291d6743b
lopatan <natalia.lopata@contractors.roche.com>
2025-05-21 22:20:53 +0200

    changed driver to RemoteWebDriver

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,21 @@
+import uuid
+
 import allure
 import pytest
+from selenium import webdriver
 from allure_commons.types import AttachmentType
-from utils.drivers import GetDriver
+from selenium.webdriver import DesiredCapabilities
+
 from config.config import Config
 
 @pytest.fixture()
 def setup(request):
-    driver = GetDriver.get_driver(Config.DEFAULT_DRIVER) # chrome is default
+    remote_url = "http://localhost:4444/wd/hub"
+
+    options = get_default_chrome_options()
+    capabilities = DesiredCapabilities.CHROME.copy()
+    driver = webdriver.Remote(command_executor=remote_url, options=options, desired_capabilities=capabilities)
+
     driver.implicitly_wait(Config.DEFAULT_TIMEOUT)
     request.cls.driver = driver
     before_failed = request.session.testsfailed
@@ -14,3 +23,19 @@ def setup(request):
     if request.session.testsfailed != before_failed:
         allure.attach(driver.get_screenshot_as_png(), name="Test failed", attachment_type=AttachmentType.PNG)
     driver.quit()
+
+    yield driver
+
+
+def get_default_chrome_options():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--no-sandbox")
+
+    options.add_argument("--disable-autofill-keyboard-accessory-view")
+    options.add_argument("--disable-save-password-bubble")
+    options.add_argument("--no-first-run")
+    options.add_argument("--disable-popup-blocking")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--user-data-dir=/tmp/selenium-profile-" + str(uuid.uuid4()))
+
+    return options
